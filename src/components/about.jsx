f29e8898d73cec88893439092533b41176876b1e
MaLasn <lasnmargareth8@gmail.com>
2025-04-15 15:47:06 +0200

    About section initial styling (mainly tackled the layout of all the items)

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,18 +1,25 @@
+import '../styles/about.scss';
+
 export default function About(){
     return( 
-    <div class="">
-    <div class="">
-        <h1 class="heading">MARGARETH LASN</h1>
-        <h4 class="heading">&lt;JUNIOR FRONTEND DEVELOPER /&gt; </h4>
-        <div class="about-me">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-            Obcaecati iure mollitia tenetur at quos quam repellat quasi 
-            quae asperiores accusantium, minus error non incidunt consectetur 
-            earum totam debitis hic praesentium!
+        <div className="container mx-auto px-4 py-8">
+            <div className='flex flex-col md:flex-row gap-4 items-stretch justify-center'>
+                <div className='flex-col content-center'>
+                    <h1 className='heading' id='name'>MARGARETH LASN</h1>
+                    <h4 className="heading" id='position'>&lt;JUNIOR FRONTEND DEVELOPER /&gt; </h4>
+                    <div className='object-fill' id="about-me">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                        Obcaecati iure mollitia tenetur at quos quam repellat quasi 
+                        quae asperiores accusantium, minus error non incidunt consectetur 
+                        earum totam debitis hic praesentium!
+                    </div>
+                </div>
+                <div className='flex items-center justify-center'>
+                    <div className='max-w-md w-full'>
+                        <img src="./images/Margareth_Lasn.jpg" alt="Margareth Lasn" id='profile-pic' className='object-cover w-full h-full'></img>
+                    </div>
+                </div>
+            </div> 
         </div>
-    </div>
-    <div class="col-6">
-        <img src="./images/Margareth_Lasn.jpg" alt="Margareth Lasn"></img>
-    </div>
-    </div> );
+    );
 }
