b6c0d58c3aa11595a4b55510e316ef07e276b609
MaLasn <lasnmargareth8@gmail.com>
2025-04-16 12:58:00 +0200

    Cleaned up the index.js and created App.jsx for more of the logic based stuff

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import React, {useState} from 'react';
+import SectionButton from './components/sectionButtons';
+import WorkExperience from './components/workExperience';
+import Education from './components/education';
+import Skills from './components/skills';
+import Projects from './components/projects';
+import Volunteering from './components/volunteering';
+import About from './components/about';
+import ContactMeButton from './components/contactMeButton';
+import ContactForm from './components/contactForm';
+
+function MainContent(){
+    const [activeSection, setActiveSection] = useState('work experience');
+  
+    const renderContent = () => {
+      if (activeSection === 'work experience') return <WorkExperience />
+      else if (activeSection === 'education') return <Education />
+      else if (activeSection === 'skills') return <Skills />
+      else if (activeSection === 'projects') return <Projects />
+      else if (activeSection === 'volunteering') return <Volunteering />
+    }
+  
+    return (
+      <>
+        <div class='content-buttons'>
+          <SectionButton activeButton={activeSection} onButtonClick={setActiveSection} />
+        </div>
+  
+        <div class="content-section">
+          {renderContent()}
+        </div>
+      </>
+    )
+  }
+
+function App() {
+    return (
+      <>
+        <ContactMeButton />
+        <About />
+        <MainContent />
+        <ContactForm />
+      </>
+    );
+  }
+  
+  export default App;
--- src/index.js
+++ src/index.js
@@ -1,52 +1,6 @@
-//for design later on fix the porportions so everything is aligned correctly
-//even by pixels not just by eye
-import React, {useState} from 'react';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
-import SectionButton from './components/sectionButtons';
-import WorkExperience from './components/workExperience';
-import Education from './components/education';
-import Skills from './components/skills';
-import Projects from './components/projects';
-import Volunteering from './components/volunteering';
-import About from './components/about';
-import ContactMeButton from './components/contactMeButton';
-import ContactForm from './components/contactForm';
 import './index.css';
+import App from './App';
 
-function MainContent(){
-  const [activeSection, setActiveSection] = useState('work experience');
-
-  const renderContent = () => {
-    if (activeSection === 'work experience') return <WorkExperience />
-    else if (activeSection === 'education') return <Education />
-    else if (activeSection === 'skills') return <Skills />
-    else if (activeSection === 'projects') return <Projects />
-    else if (activeSection === 'volunteering') return <Volunteering />
-  }
-
-  return (
-    <>
-      <div class='content-buttons'>
-        <SectionButton activeButton={activeSection} onButtonClick={setActiveSection} />
-      </div>
-
-      <div class="content-section">
-        {renderContent()}
-      </div>
-    </>
-  )
-}
-
-const rootHeader = createRoot(document.getElementById('header'));
-rootHeader.render(<ContactMeButton />);
-
-const rootAboutMe = createRoot(document.getElementById('aboutMe'));
-rootAboutMe.render(<About />);
-
-const rootMainContent = createRoot(document.getElementById('resumeContent'));
-rootMainContent.render(<MainContent />)
-
-const rootContactForm = createRoot(document.getElementById('contactForm'));
-rootContactForm.render(<ContactForm />);
-
-
+createRoot(document.getElementById('root')).render(<App />)
