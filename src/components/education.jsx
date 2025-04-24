ae2e5a7bef248e839f6e9313dcb81ad9ca7c1f5a
MaLasn <lasnmargareth8@gmail.com>
2025-04-24 20:44:52 +0200

    Updated education section, still need to update the erasmus experience and add detail

--- src/components/education.jsx
+++ src/components/education.jsx
@@ -18,24 +18,14 @@ export default function Education(){
             <div class='experience-paragraph'>
                 <h5>TALLINN UNIVERSITY OF TECHNOLOGY</h5>
                 <p><span>BUSINESS INFORMATION TECHNOLOFY MASTER'S DEGREE</span><span>2023 - PRESENT</span></p>
-                <p class='text'>Lorem Ipsum is simply dummy text of the printing and  typesetting industry. 
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                when an unknown printer took a galley of  type and scrambled it to make a type 
-                specimen book. It has survived not  only five centuries, but also the leap into 
-                electronic typesetting,  remaining essentially unchanged. It was popularised in 
-                the 1960s with  the release of Letraset sheets containing Lorem Ipsum passages, 
-                and more recently with desktop.</p>
+                <p class='text'>Shifted focus toward frontend development by taking elective courses in UI/UX 
+                    design and web development.</p>
             </div>
             <div class='experience-paragraph'>
                 <h5>TALLINN UNIVERSITY OF TECHNOLOGY</h5>
                 <p><span>BUSINESS INFORMATION TECHNOLOFY BACHELOR'S DEGREE</span><span>2020 - 2023</span></p>
-                <p class='text'>Lorem Ipsum is simply dummy text of the printing and  typesetting industry. 
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                when an unknown printer took a galley of  type and scrambled it to make a type 
-                specimen book. It has survived not  only five centuries, but also the leap into 
-                electronic typesetting,  remaining essentially unchanged. It was popularised in 
-                the 1960s with  the release of Letraset sheets containing Lorem Ipsum passages, 
-                and more recently with desktop.</p>
+                <p class='text'>Gained base programming skills with HTML, CSS, and JavaScript through hands-on coursework.
+                First exposure to frontend programming that sparked a long-term interest in creating user interfaces.</p>
             </div>
         </div>
     );
