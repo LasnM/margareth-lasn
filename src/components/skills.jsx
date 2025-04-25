e0f20e56d83c8b730289c51e6e209e7801a86fcf
MaLasn <lasnmargareth8@gmail.com>
2025-04-25 18:46:52 +0200

    Updated the skills

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -3,31 +3,29 @@ import '../styles/content.scss';
 export default function Skills(){
     return (
         <div class="skillset">
-            <div class="grid md:grid-cols-3 gap-4">
+            <div class="grid grid-cols-3 gap-4">
                 <div class="experience-paragraph">
                     <h5 class='list-title flex justify-center'>PROGRAMMING</h5>
                     <ul>
-                        <li className='flex justify-center'>JAVASCRIPT (COMPETENT)</li>
-                        <li className='flex justify-center'>HTML (COMPETENT)</li>
-                        <li className='flex justify-center'>CSS (COMPETENT)</li>
-                        <li className='flex justify-center'>JAVA (ENTRY-LEVEL)</li>
-                        <li className='flex justify-center'>C# (ENTRY-LEVEL)</li>
+                        <li className='flex justify-center'>JavaScript (Competent)</li>
+                        <li className='flex justify-center'>HTML (Competent)</li>
+                        <li className='flex justify-center'>CSS (Comptetent)</li>
                     </ul>
                 </div>
                 <div class="experience-paragraph">
                 <h5 class='list-title flex justify-center'>TOOLS</h5>
                     <ul>
-                        <li className='flex justify-center'>FIGMA (COMPETENT)</li>
-                        <li className='flex justify-center'>VSCODE (COMPETENT)</li>
-                        <li className='flex justify-center'>GIT & GITHUB (COMPETENT)</li>
+                        <li className='flex justify-center'>FIGMA (Competent)</li>
+                        <li className='flex justify-center'>VSCODE (Competent)</li>
+                        <li className='flex justify-center'>GIT (Competent)</li>
                     </ul>
                     </div>
                 <div class="experience-paragraph">
-                <h5 class='list-title flex justify-center'>FRAMEWORKS & LIBRARIES</h5>
+                <h5 class='list-title flex justify-center'>FRAMEWORKS</h5>
                     <ul>
-                        <li className='flex justify-center'>BOOTSRAP (COMPETENT)</li>
-                        <li className='flex justify-center'>TAILWIND (ENTRY-LEVEL)</li>
-                        <li className='flex justify-center'>REACT (ENTRY-LEVEL)</li>
+                        <li className='flex justify-center'>BOOTSRAP (Competent)</li>
+                        <li className='flex justify-center'>TAILWIND (Competent)</li>
+                        <li className='flex justify-center'>REACT (Entry-level)</li>
                     </ul>
                 </div>
             </div>
