8255bb1b5e4d81b6e825b0d913d49b8e3ef3ba7c
MaLasn <lasnmargareth8@gmail.com>
2025-04-27 17:57:32 +0200

    Skills view is done

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -1,37 +1,44 @@
 import '../styles/content.scss';
-import {frontendSkills, backendSkills} from '../data/skillsData';
+import frontendSkills from '../data/frontendSkillsData';
+import backendSkills from '../data/backendSkillsData';
 
 export default function Skills(){
     
 
     return (
-        <div id='skillset'>
-            <div class="card-content-section">
-            <h5>FRONTEND</h5>
-            <div>COMPETENT</div>
-            {frontendSkills.map((skill, index) => (
-                skill.level === 'COMPETENT' ? (<div className='skill'>
-                    <img src={skill.imageLink} alt={skill.altText}></img>
-                    <div className='skill-name'>{skill.title}</div>
-                </div>) : ''
-            ))}
-            <div>ENTRY-LEVEL</div>
-            {frontendSkills.map((skill, index) => (
-                skill.level === 'ENTRY-LEVEL' ? (<div className='skill'>
-                    <img src={skill.imageLink} alt={skill.altText}></img>
-                    <div className='skill-name'>{skill.title}</div>
-                </div>) : ''
-            ))}
+        <div className='flex flex-wrap justify-center'>
+            <div className="card-content-section experience-paragraph">
+                <h5 className='flex justify-center'>FRONTEND</h5>
+                <div className='level flex justify-center'>COMPETENT</div>
+                <div className='skillset'>
+                    {frontendSkills.map((skill, index) => (
+                        skill.level === 'COMPETENT' ? (<div className='skills flex flex-col items-center' id={skill.title}>
+                            <img src={skill.imageLink} alt={skill.altText} className='logos'></img>
+                            <div className='skill-name'>{skill.title}</div>
+                        </div>) : ''
+                    ))}
+                </div>
+                <div className='level flex justify-center'>ENTRY-LEVEL</div>
+                <div className="skillset">
+                    {frontendSkills.map((skill, index) => (
+                        skill.level === 'ENTRY-LEVEL' ? (<div className='skills flex flex-col items-center'>
+                        <img src={skill.imageLink} alt={skill.altText} className='logos'></img>
+                        <div className='skill-name'>{skill.title}</div>
+                        </div>) : ''
+                    ))}
+                </div>
             </div>
-            <div class='card-contemt-section'>
-                <h5>BACKEND</h5>
-                <div>ENTRY-LEVEL</div>
+            <div className='card-content-section experience-paragraph'>
+                <h5 className='flex justify-center'>BACKEND</h5>
+                <div className='level flex justify-center'>ENTRY-LEVEL</div>
+                <div className="skillset">
                 {backendSkills.map((skill, index) => (
-                skill.level === 'ENTRY-LEVEL' ? (<div className='skill'>
-                    <img src={skill.imageLink} alt={skill.altText}></img>
+                skill.level === 'ENTRY-LEVEL' ? (<div className='skills flex flex-col items-center'>
+                    <img src={skill.imageLink} alt={skill.altText} className='logos'></img>
                     <div className='skill-name'>{skill.title}</div>
                 </div>) : ''
             ))}
+                </div>
             </div>
         </div>
     );
