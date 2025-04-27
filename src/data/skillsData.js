de09d21ace47d1279926961924780231b7bd9419
MaLasn <lasnmargareth8@gmail.com>
2025-04-27 17:01:21 +0200

    Skills data in a object

---
+++ src/data/skillsData.js
@@ -0,0 +1,67 @@
+const skills = [
+    {
+        title: 'HTML',
+        field: 'FRONTEND',
+        level: 'COMPETENT',
+        imageLink: 'https://img.icons8.com/color/48/html-5--v1.png',
+        altText: 'HTML logo'
+    },
+    {
+        title: 'CSS',
+        field: 'FRONTEND',
+        level: 'COMPETENT',
+        imageLink: 'https://img.icons8.com/fluency/48/css3.png',
+        altText: 'CSS logo'
+    },
+    {
+        title: 'JavaScript',
+        field: 'FRONTEND',
+        level: 'COMPETENT',
+        imageLink: 'https://img.icons8.com/color/48/javascript--v1.png',
+        altText: 'JavaScript logo'
+    },
+    {
+        title: 'Bootstrap',
+        field: 'FRONTEND',
+        level: 'ENTRY-LEVEL',
+        imageLink: 'https://img.icons8.com/color/48/bootstrap--v2.png',
+        altText: 'Bootstrap logo'
+    },
+    {
+        title: 'React',
+        field: 'FRONTEND',
+        level: 'ENTRY-LEVEL',
+        imageLink: 'https://img.icons8.com/officel/80/react.png',
+        altText: 'React logo'
+    },
+    {
+        title: 'Tailwind CSS',
+        field: 'FRONTEND',
+        level: 'ENTRY-LEVEL',
+        imageLink: 'https://img.icons8.com/fluency/48/tailwind_css.png',
+        altText: 'Tailwind CSS logo'
+    },
+    {
+        title: 'Sass',
+        field: 'FRONTEND',
+        level: 'ENTRY-LEVEL',
+        imageLink: 'https://img.icons8.com/color/48/sass.png',
+        altText: 'Sass logo'
+    },
+    {
+        title: 'Java',
+        field: 'BACKEND',
+        level: 'ENTRY-LEVEL',
+        imageLink: 'https://img.icons8.com/color/48/java-coffee-cup-logo--v1.png',
+        altText: 'Java logo'
+    },
+    {
+        title: 'C#',
+        field: 'BACKEND',
+        level: 'ENTRY-LEVEL',
+        imageLink: 'https://img.icons8.com/color/48/c-sharp-logo-2.png',
+        altText: 'C# logo'
+    }    
+]
+
+export default skills;
